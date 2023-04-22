adf8c5d54bf658ed7dc5625e18f86ce1539b9944
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-04-21 18:11:35 -0600

    Refactor: Temporarily remove API data to avoid hitting rate limit

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -4,11 +4,11 @@ require './app/services/unsplash_service'
 require './app/poros/photo'
 
 class PhotoService
-  def self.service
-    UnsplashService.new
-  end
+  # def self.service
+  #   UnsplashService.new
+  # end
 
-  def self.photo(item)
-    Photo.new(service.photo(item))
-  end
+  # def self.photo(item)
+  #   Photo.new(service.photo(item))
+  # end
 end
